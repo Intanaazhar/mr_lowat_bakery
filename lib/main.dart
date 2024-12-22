e89f7161830a33d1ac882b40e8419e9d5e08d28c
zfrtlsofea <sofea306@yahoo.com>
2024-12-22 23:55:18 +0800

    Revert "Dah setup firebase (android, ios, web)"
    This reverts commit 627121d6e00887d7666f714ede189107259a3f23.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mr_lowat_bakery/userscreens/welcome.dart';
-import 'package:firebase_core/firebase_core.dart'; //dpt from firebase setup
-
 // ignore: unused_import
 import 'welcome.dart'; // Import your welcome screen file
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: FirebaseOptions(
-            apiKey: "AIzaSyDzTktXgjlgEqd8E6t0k6HzWv03xyBErFY",
-            authDomain: "mr-lowat-bakery.firebaseapp.com",
-            projectId: "mr-lowat-bakery",
-            storageBucket: "mr-lowat-bakery.firebasestorage.app",
-            messagingSenderId: "109927117800",
-            appId: "1:109927117800:web:86c4c70da6b56b87e0aa5d"));
-  } else {
-    await Firebase.initializeApp();
-  }
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -35,8 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange, // Set your primary color theme
       ),
-      home:
-          const BakeryWelcomeScreen(), // Set your welcome screen as the home page
+      home: const BakeryWelcomeScreen(), // Set your welcome screen as the home page
     );
   }
 }
