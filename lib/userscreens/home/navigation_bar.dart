340bedec5c759d0346dca2113ac3dcaf77fd739d
Intanaazhar <84037@siswa.unimas.my>
2025-01-05 16:02:19 +0800

    latest commit 3.0

--- lib/userscreens/home/navigation_bar.dart
+++ lib/userscreens/home/navigation_bar.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:mr_lowat_bakery/userscreens/home/homepage.dart';
+import 'package:mr_lowat_bakery/userscreens/home/profile_menu.dart';
 import 'package:mr_lowat_bakery/userscreens/home/search_page.dart';
-import 'package:mr_lowat_bakery/userscreens/home/user_profile.dart';
+
 
 
 class NavigationMenu extends StatelessWidget {
@@ -38,5 +39,5 @@ Widget build(BuildContext context) {
 class NavigationController extends GetxController{
   final Rx<int> selectedIndex = 0.obs;
 
-  final screens = [const Homepage(),const SearchPage(),Container(color: Colors.orange), Container(color: Colors.orange), const UserProfile()];
+  final screens = [const Homepage(),const SearchPage(),Container(color: Colors.orange), Container(color: Colors.orange), const ProfileMenu()];
 }
